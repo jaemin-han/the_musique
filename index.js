0bce4bb3f95342c9595a7671624e02603dd67863
Jaemin Han <jaemin.han@nyu.edu>
2017-01-08 23:58:38 -0500

    files and index.js update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const dotenv = require('dotenv').config({ silent: true });
+// const dotenv = require('dotenv').config({ silent: true });
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 // Routes folder
-// const indexRouter = require('./routes/index.js');
+const indexRouter = require('./routes/index.js');
+const authRouter = require('./routes/auth.js');
+const musicRouter = require('./routes/music.js');
+const usersRouter = require('./routes/users.js');
 
 const app = express();
 
@@ -22,7 +25,11 @@ app.use(bodyParser.json());
 // Set static file root folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+// Routes -- subpages
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/music', musicRouter);
+app.use('/users', usersRouter);
 
 // Port listening for connections
 // Sidenote: 'process.env.PORT' is needed for when we deploy to Heroku
