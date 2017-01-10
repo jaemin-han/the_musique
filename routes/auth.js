6393d10f5a6e5c62c824f12a1c099e89eb499fba
Jaemin Han <jaemin.han@nyu.edu>
2017-01-10 14:32:01 -0500

    ejs and userauth

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { logIn } = require('../lib/auth');
+
+// Router
+const authRouter = express.Router();
+
+// Log in and if successful, assign res.user_id to the session
+// 'logIn' middleware from the ../lib/auth to parse the form input
+// and save user to the database
+authRouter.post('/', logIn, (req, res) => {
+  res.redirect('/users/profile');
+});
+
+authRouter.delete('/', (req, res) => {
+  req.session.userId = null;
+  res.redirect('/');
+});
+
+module.exports = authRouter;
