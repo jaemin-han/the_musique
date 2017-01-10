0d6c2c1be2785636a0209724d0127c0ee1fd8a0a
Jaemin Han <jaemin.han@nyu.edu>
2017-01-10 15:10:26 -0500

    brief adjustment

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// const dotenv = require('dotenv').config({ silent: true });
+const dotenv = require('dotenv').config({ silent: true });
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+
 
 // Routes folder
 const indexRouter = require('./routes/index.js');
@@ -11,9 +15,11 @@ const musicRouter = require('./routes/music.js');
 const usersRouter = require('./routes/users.js');
 
 const app = express();
+const SECRET = 'music3000';
 
 app.set('view engine', 'ejs');
 
+// log request
 app.use(morgan('dev'));
 
 // Parse application
@@ -21,6 +27,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse application/json
 app.use(bodyParser.json());
+
+// Read the cookies sent over from the browser
+app.use(cookieParser());
+
+app.use(session({
+  reserve: false,
+  saveUninitialized: false,
+  secret: SECRET,
+}));
 
 // Set static file root folder
 app.use(express.static(path.join(__dirname, 'public')));
